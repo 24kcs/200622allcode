e7ecfea44a0356d7cafd432b8499c212f17a69f1
24kcs <younghongbo@126.com>
2020-09-01 11:54:28 +0800

    02codes

---
+++ 02-code/01-vscode的介绍及使用/01-vscode笔记.js
@@ -0,0 +1,17 @@
+/*
+1.vscode 编辑工具
+官方下载地址: https://code.visualstudio.com/Download
+中文插件: Chinese (Simplified) Language Pack for VS Code
+主题插件: Material Theme
+其他插件: 
+https://segmentfault.com/a/1190000006697219?utm_source=tag-newest
+https://www.cnblogs.com/zhaoshujie/p/9834654.html
+https://blog.fundebug.com/2018/07/24/vs-extensions/
+快捷键: Ctrl+Shift+P  利用搜索的方式进行设置
+补全插件: Auto Rename Tag 
+自动刷新浏览器: Live Server
+右键菜单在浏览器中打开: open in browser
+设置编辑内容缩放: 文件--->首选项--->设置 ,搜索: Mouse Wheel Zoom 勾选即可
+
+
+*/
